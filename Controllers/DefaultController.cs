c41f8ec660d2a4c63a69ec6c72d1116cc59e12c4
slhnklnc <aslhanklnc@gmail.com>
2021-11-28 19:34:04 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+namespace WepService.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [WebMethod]
+        public JsonResult getUser()
+        {
+            DataModel dm = new DataModel();
+            var userLst = dm.User.ToList();
+            return Json(userLst);
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WepService.WebApiApplication" Language="C#" %>
